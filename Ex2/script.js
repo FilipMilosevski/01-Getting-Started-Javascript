3a28d0b48137eb7e521028b6454cd75eff7a2964
FilipMilosevski <filipdh94@gmail.com>
2022-11-17 18:59:46 +0100

    2

---
+++ Ex2/script.js
@@ -0,0 +1,11 @@
+alert ("filip");
+
+  // kosdkadwo kdkowa dow kdkw od ka
+
+
+  // ijdiwadjwiajd
+  // dawiodjwa
+  // debuggerjiwjdawijdiajd diaw jdij awidajw
+  // jdi wjad iawjdijaw idawjidwaji dawiodjwaj idawjidwajijd jdij
+  // dowakdaw 
+  // kdowakdoawkd
