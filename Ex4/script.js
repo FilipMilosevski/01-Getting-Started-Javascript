f51f46b11b8c319bd174d003838204241eed5810
FilipMilosevski <filipdh94@gmail.com>
2022-11-18 16:21:40 +0100

    1

---
+++ Ex4/script.js
@@ -0,0 +1,63 @@
+// console.log ("FILIP");
+
+// prompt ("FILIP");
+
+/*
+Multi 
+Line 
+Comment
+*/
+
+
+
+/* let userName = prompt ("Insert your name");
+
+alert ("vaseto ime " +userName +" e procesirano");
+console.log (userName);
+*/
+
+
+
+
+
+
+
+/*
+let randomNum = Math.random () * 3;
+console.log ("My random number: " + randomNum);
+let roundNum = Math.floor (randomNum) + 1;
+console.log ("My rounded number: " + roundNum);
+
+
+
+let num= prompt ("Guess a number from 1 to 3");
+
+
+if (roundNum ==num) {
+  alert ("you guessed right!")
+} 
+else {
+  alert ("you guessed wrong!")
+}
+*/
+
+
+
+
+
+
+let firstname = "maria";
+console.log (firstname);
+firstname= "jacky";
+console.log (firstname);
+
+
+
+
+/*
+let singleString = 'hi';
+let doubleString = 'aaaa';
+console.log (singleString);
+*/
+
+let usw= prompt ("insert");
