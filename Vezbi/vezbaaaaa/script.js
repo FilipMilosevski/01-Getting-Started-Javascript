8ec0afda25a3a15a1deceadc926da06b7f521f3c
FilipMilosevski <filipdh94@gmail.com>
2022-11-20 11:34:44 +0100

    1

---
+++ Vezbi/vezbaaaaa/script.js
@@ -0,0 +1,22 @@
+// let a =10;
+// let b =20;
+
+// let c = a + b;
+
+// console.log(c);
+
+
+// console.log ("The sum of "+ a +" and " + b + "is: " + (a + b));
+
+
+
+
+let a = 50;
+let b =100;
+let c = a + b + b + b + b + b;
+
+
+
+
+
+console.log (c);
